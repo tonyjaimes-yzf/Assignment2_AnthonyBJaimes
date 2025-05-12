ad7711c48e67aa37ee30fc805b4731912d2e75ec
anthonyjaimes9 <ajaimes9@depaul.edu>
2025-05-12 15:53:09 -0500

    Character subclasses Monster is implemented as a monster

---
+++ src/main/java/org/example/Monster.java
@@ -0,0 +1,11 @@
+package org.example;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Monster extends Character {
+    Monster(String name, int strength, int craft, int health) {
+        super(name, strength, craft, health);
+    }
+}
