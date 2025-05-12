9bc4760dd7f3c456d123bc4321b70cbdb74c43f4
anthonyjaimes9 <ajaimes9@depaul.edu>
2025-05-12 15:52:47 -0500

    Character subclasses Wizard, Warrior, implemented as playable characters

---
+++ src/main/java/org/example/Warrior.java
@@ -0,0 +1,12 @@
+package org.example;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Warrior extends Character {
+
+    public Warrior(String name) {
+        super(name, 20, 10, 10);
+    }
+}
---
+++ src/main/java/org/example/Wizard.java
@@ -0,0 +1,12 @@
+package org.example;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Wizard extends Character {
+
+    public Wizard(String name) {
+        super(name, 1, 4, 3);
+    }
+}
