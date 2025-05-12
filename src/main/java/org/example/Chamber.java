d2653af14ed74ebeee3b5ed9645cab9d7a3d3e78
anthonyjaimes9 <ajaimes9@depaul.edu>
2025-05-12 15:53:45 -0500

    Chamber class implemented

---
+++ src/main/java/org/example/Chamber.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Chamber {
+    private List<Door> doors;
+    private List<Item> items;
+
+    public Chamber() {
+        this.doors = new ArrayList<>();
+        this.items = new ArrayList<>();
+    }
+    public Chamber(Item item) {
+        this();
+        this.items.add(item);
+    }
+    public List<Door> getDoors() {
+        return doors;
+    }
+    public List<Item> getItems() {
+        return items;
+    }
+    public void addDoor(Door door) {
+        doors.add(door);
+    }
+}
