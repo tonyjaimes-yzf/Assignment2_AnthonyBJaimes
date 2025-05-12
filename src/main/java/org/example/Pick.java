38d2aa050713b8c9ffd40ece18d015b341db2993
anthonyjaimes9 <ajaimes9@depaul.edu>
2025-05-12 15:54:49 -0500

    Move class implemented

---
+++ src/main/java/org/example/Pick.java
@@ -0,0 +1,26 @@
+package org.example;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Pick extends Action {
+    private Item item;
+    public Pick(Dungeon dungeon, Item item) {
+        super(dungeon);
+        this.item = item;
+    }
+    public void execute() {
+        dungeon.getPlayer().addItem(item);
+        Character player = dungeon.getPlayer();
+        dungeon.getCurrentChamber().getItems().remove(item);
+        System.out.println("You got a new item!  ");
+        System.out.println("Item: " + item.getName());
+        System.out.println("Your Item Added: Strength: " + item.getStrength() + ", Craft: " + item.getCraft());
+        System.out.println("Character Stats: Strength: " + player.getCharStrength() + ", Craft: " + player.getCharCraft() + ", Health: " + player.getCharHealth()+"\n");
+    }
+
+    public String toString() {
+        return "Pick up a " + item.getName();
+    }
+}
