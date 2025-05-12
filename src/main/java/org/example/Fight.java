b73ac8ce79aee2592b812f8f14739e47478102cd
anthonyjaimes9 <ajaimes9@depaul.edu>
2025-05-12 15:54:31 -0500

    Fight class implemented

---
+++ src/main/java/org/example/Fight.java
@@ -0,0 +1,54 @@
+package org.example;
+import java.util.Random;
+//------------------------------------------------------
+// Assignment 2
+// Written by: Anthony Jaimes ID: 2018391
+// For SES350 Section 602 – Spring 2025
+//--------------------------------------------------------
+public class Fight extends Action {
+    private final Monster monster;
+
+    public Fight(Dungeon dungeon, Monster monster) {
+        super(dungeon);
+        this.monster = monster;
+    }
+    public void execute() {
+        Character player = dungeon.getPlayer();
+        Random rand = new Random();
+        System.out.println("You must defeat the Monster before going through!");
+        System.out.println("Fighting a " + monster.getCharName() + "!");
+
+        while (player.getCharHealth() > 0 && monster.getCharHealth() > 0) {
+            int playersTurn = rand.nextInt(6) + 1;
+            int playersLevel = player.getCharStrength() + playersTurn;
+            System.out.println("Player rolled a: " + playersTurn);
+            System.out.println("Total Power: " + playersLevel);
+            int monstersTurn = rand.nextInt(6) + 1;
+            int monstersLevel = monster.getCharStrength() + monstersTurn;
+            System.out.println("Monster rolled a: " + monstersTurn);
+            System.out.println("Total Power: " + monstersLevel);
+
+            if (playersLevel > monstersLevel) {
+                monster.reduceHealth(playersLevel - monstersLevel);
+                System.out.println("You hit the Monster, Monster health: " + monster.getCharHealth());
+            } else {
+                player.reduceHealth(monstersLevel - playersLevel);
+                System.out.println("You got hit, Your health: " + player.getCharHealth());
+            }
+        }
+        if (monster.getCharHealth() <= 0) {
+            System.out.println("You killed the monster!");
+            dungeon.getCurrentChamber()
+                    .getDoors()
+                    .stream()
+                    .filter(door -> door.getGuard() == monster)
+                    .findFirst()
+                    .ifPresent(Door::defeatGuard);
+        } else {
+            System.out.println("You Died!");
+        }
+    }
+    public String toString() {
+        return "Fight " + monster.getCharName();
+    }
+}
